125af6815929c53468c5456b381dbb0edabcb5d7
rogenis <rogenis.silva@meliuz.com.br>
2022-08-15 17:56:47 -0400

    config: Configuracao do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+        "quotes": ["error", "single"],
+        "indent": ["error", 4],
+        "comma-spacing": ["error", { befora: false, after: true }],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    }
+}
